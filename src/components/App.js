76946faf682bef47f24709e77870bc145896e249
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-03 21:24:02 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import NavBar from './NavBar'
 // the App component should render out the GifListContainer component 
 import GifListContainer from '../containers/GifListContainer'
 
+
 const App = () => {
   return (
     <div>
